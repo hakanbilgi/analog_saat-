5b13239aae891a6d4ca6636ae700d81ab4721096
Hakan Bilgi <hknblg34@gmail.com>
2022-09-25 20:47:57 +0300

    setInterval

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,16 @@
  const sec = document.querySelector(".secs");
  const min = document.querySelector(".mins");
  const hours = document.querySelector(".hours");
+
+ function tiktak (){
+
+
+
+
+
+
+
+
+ }
+
+ setInterval(tiktak,1000);
