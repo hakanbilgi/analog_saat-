7b6ba7fc6156cab6c6a0d287bfe5c6ae0c5b5674
Hakan Bilgi <hknblg34@gmail.com>
2022-09-25 21:05:08 +0300

    saat

--- javaScript.js
+++ javaScript.js
@@ -7,6 +7,10 @@ let seconds = new Date().getSeconds();
 let minute = new Date().getMinutes();
 let hour = new Date().getHours();
 
+sec.style.transform = `rotade(${180}deg)`
+min.style.transform = `rotade(${180}deg)`
+hours.style.transform = `rotade(${180}deg)`
+
 
 
 
