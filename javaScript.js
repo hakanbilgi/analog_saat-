4ecb99f4dc7ee01a8cfe0a5d235ffdcff6fbf741
Hakan Bilgi <hknblg34@gmail.com>
2022-09-25 21:23:43 +0300

    colors

--- javaScript.js
+++ javaScript.js
@@ -1,23 +1,17 @@
- const sec = document.querySelector(".secs");
- const min = document.querySelector(".mins");
- const hours = document.querySelector(".hours");
+const sec = document.querySelector(".secs");
+const min = document.querySelector(".mins");
+const hours = document.querySelector(".hours");
 
- function tiktak (){
-let second = new Date().getSeconds();
-let minute = new Date().getMinutes();
-let hour = new Date().getHours();
+function tiktak() {
+  let second = new Date().getSeconds();
+  let minute = new Date().getMinutes();
+  let hour = new Date().getHours();
 
-sec.style.transform = `rotate(${180 + (second * 6)}deg)`
-min.style.transform = `rotate(${180 + (minute * 6)}deg)`
-hours.style.transform = `rotate(${180 + (hour * 30)}deg)`
+  sec.style.transform = `rotate(${180 + second * 6}deg)`;
+  min.style.transform = `rotate(${180 + minute * 6}deg)`;
+  hours.style.transform = `rotate(${180 + hour * 30}deg)`;
 
+  
+}
 
-
-
-
-
-
-
- }
-
- setInterval(tiktak,1000);
+setInterval(tiktak, 1000);
