0a99ce3a53b3974070c86771072677b2f492026d
Hakan Bilgi <hknblg34@gmail.com>
2022-09-25 21:12:43 +0300

    hours

--- javaScript.js
+++ javaScript.js
@@ -3,13 +3,13 @@
  const hours = document.querySelector(".hours");
 
  function tiktak (){
-let seconds = new Date().getSeconds();
+let second = new Date().getSeconds();
 let minute = new Date().getMinutes();
 let hour = new Date().getHours();
 
-sec.style.transform = `rotade(${180}deg)`
-min.style.transform = `rotade(${180}deg)`
-hours.style.transform = `rotade(${180}deg)`
+sec.style.transform = `rotate(${180 + (second * 6)}deg)`
+min.style.transform = `rotate(${180 + (minute * 6)}deg)`
+hours.style.transform = `rotate(${180 + (hour * 30)}deg)`
 
 
 
